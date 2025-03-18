04f3a004a2e8a4dcc015b58e31ce6982f79eb8d8
WesleiKhan <wesleikhan@gmail.com>
2025-03-18 13:32:41 -0300

    first commit

---
+++ src/main/java/com/controle_de_assinaturas/ControleDeAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package com.controle_de_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controle_de_assinaturas/ControleDeAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controle_de_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
